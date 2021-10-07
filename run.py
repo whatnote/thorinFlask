caa06f2b45d754ac79004ba76036b5e66bb2a234
DANIEL <daniel_cornwall@hotmail.com>
2021-10-07 23:41:21 +0000

    boot stap styling added

--- run.py
+++ run.py
@@ -20,6 +20,10 @@ def contact():
     return render_template("contact.html")
 
 
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")
+
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
